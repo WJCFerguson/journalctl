3c9e1a2410eab1d2a0d02c4aa8f4a7b9273a6373
James Ferguson <wjcferguson@gmail.com>
2023-10-22 12:26:22 -0400

    v1.0.1

--- journalctl-mode.el
+++ journalctl-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: James Ferguson <james@faff.org>
 ;; Keywords: lisp
-;; Version: 1.0
+;; Version: 1.0.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
